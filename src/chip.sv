0dc229b6f2def5d61a81125d3b23ea41ef063559
RomanMakingCode <89269157+RomanMakingCode@users.noreply.github.com>
2023-05-05 18:24:12 -0400

    Update chip.sv

--- src/chip.sv
+++ src/chip.sv
@@ -1,5 +1,4 @@
 `default_nettype none
-
 module my_chip (
     input logic [11:0] io_in, // Inputs to your chip
     output logic [11:0] io_out, // Outputs from your chip
@@ -7,6 +6,6 @@ module my_chip (
     input logic reset // Important: Reset is ACTIVE-HIGH
 );
     
-    mychip_wrapper wrapper (io_in[11:0], io_out[11:0], clock, reset); 
+    mychip_wrapper wrapper (.mode_sel(io_in[1:0]),.display_sel(io_in[5:2]),.in_bit(io_in[6]),.ready(io_in[7]), .hex_out(io_out[7:0]), .clock(clock), .reset(reset)); 
     
 endmodule
